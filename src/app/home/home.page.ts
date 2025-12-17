0cfecd3c28e605504c2068a96ee9a4e1410b3dfc
VasylMeln <BasilTech@proton.me>
2025-12-17 23:32:22 +0000

    starting with homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonButton, IonInput, IonItem, IonList } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonInput, IonItem, IonList, IonButton, FormsModule],
 })
 export class HomePage {
   constructor() {}
+
+  userIngredients: string = '';
+  ingredientInput: string = '';
+
+  Search() {
+    this.userIngredients=this.ingredientInput;
+    
+
+  }
 }
